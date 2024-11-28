0b889f6045b192f3cf7e9de25dafcf07853a156d
author name <test@test.test>
2024-11-28 10:46:55 +0100

    Fix mistakes

--- controllers/homepage.php
+++ controllers/homepage.php
@@ -3,7 +3,9 @@ include_once __DIR__ . "/../models/sales.php";
 include_once __DIR__ . "/../views/homepage.php";
 
 function execute(){
-    SalesModel::initializeDatabase();
-    $mainSale = SalesModel::getMainSale();
+    initializeDatabase();
+    $mainSale = getMainSale();
     HomepageView::Render($mainSale);
 }
+
+execute();
